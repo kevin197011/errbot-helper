19125a6dae9cb6266fe1b5835565f464a483de5c
kk <61411931+kevin197011@users.noreply.github.com>
2024-04-16 14:07:22 +0800

    Update baseUtil.py

--- lib/baseUtil.py
+++ lib/baseUtil.py
@@ -4,14 +4,17 @@
 # https://opensource.org/licenses/MIT
 
 from dotenv import load_dotenv
+from abc import ABC, abstractmethod
 
 
-class BaseUtil:
+class BaseUtil(ABC):
     def __init__(self):
         load_dotenv()
 
+    @abstractmethod
     def _instance(self):
         pass
 
+    @abstractmethod
     def run(self):
         pass
